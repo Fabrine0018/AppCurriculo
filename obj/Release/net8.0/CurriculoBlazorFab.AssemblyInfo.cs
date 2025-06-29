4a5fdda9d95f84f8e96db3bad95591ddc3ed5472
Fabrine0018 <fabrine.velame@gmail.com>
2025-06-28 20:09:36 -0400

    Ajustes no counter
    Educação, Experiencia, e habilidades

--- obj/Release/net8.0/CurriculoBlazorFab.AssemblyInfo.cs
+++ obj/Release/net8.0/CurriculoBlazorFab.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurriculoBlazorFab")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d30bfe7ff54e7555bcbd45aee44fd4b239b16f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurriculoBlazorFab")]
